2d248caf5d5548b84b1db03b883717a5a9ef7479
Takuma Shimizu <tashimiz@student.42tokyo.jp>
2022-07-17 20:04:36 +0900

    [Add/Update] Add and update project for hosting media files #1
    Added and  update project setting for hosting and uploading media
    files.

--- app/config/settings.py
+++ app/config/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'upload',
 ]
 
 MIDDLEWARE = [
@@ -138,3 +140,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
